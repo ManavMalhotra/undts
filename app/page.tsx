ba2eb2c9b2d8a6dab800c2d33857da060834c132
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2024-05-23 23:52:26 +0530

    added carousel and updated styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,94 @@
+import Carousel from "@/components/Carousel";
 import ClientCard from "@/components/ClientCard";
+import EmblaCarousel from "@/components/EmblaCarousel";
+import MainCarousel from "@/components/MainCarousel";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import { useState } from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { IoMdAlarm } from "react-icons/io";
+import { IoDiamondOutline, IoHelpBuoyOutline } from "react-icons/io5";
 import { MdArrowLeft, MdArrowRight, MdSafetyCheck } from "react-icons/md";
+import { PiDropBold } from "react-icons/pi";
+import project1 from "@/app/Imageproject1.png"
 
 
 export default function page() {
+
+
+  const images = [project1, project1, project1, project1];
+
+
   return (
     <>
       <div>
         <Navbar />
       </div>
 
-      <main className="text-center px-8">
+      <main className="px-16 py-16 text-center">
         <h3 className="text-base">Since 1990</h3>
-        <h1 className="text-2xl">
+        <h1 className="text-2xl md:text-5xl md:px-72">
           India's most reliable corrosion prevention service company
         </h1>
       </main>
 
+      <Carousel loop>
+        {images.map((src, i) => {
+          return (
+            // 👇 style each individual slide.
+            // relative - needed since we use the fill prop from next/image component
+            // h-64 - arbitrary height
+            // flex[0_0_100%]
+            //   - shorthand for flex-grow:0; flex-shrink:0; flex-basis:100%
+            //   - we want this slide to not be able to grow or shrink and take up 100% width of the viewport.
+            <div className="relative max-h-full h-[40vw] w-full flex-[0_0_100%]" key={i}>
+              {/* use object-cover + fill since we don't know the height and width of the parent */}
+              <Image src={src} fill className="object-fill" alt="alt" />
+            </div>
+          );
+        })}
+      </Carousel>
+
       {/* Reliable Section  */}
-      <section className="px-8">
-        <h1 className="text-2xl">
-          What makes us reliable company ?
-        </h1>
+      <section className="flex flex-col gap-4 px-8 py-8 md:px-16 md:flex-row">
 
-        <section className="flex flex-wrap gap-4">
-          <div className="flex-[1/2]">
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">Safety</h4>
+        <div className="md:w-2/5 md:flex md:justify-between md:flex-col md:pr-8">
+          <h1 className="text-2xl md:text-4xl">
+            What makes us reliable company ?
+          </h1>
+          <div className="items-center hidden text-red-800 md:flex md:gap-2">
+            <h1>Schedule free consultation </h1>
+            <FaArrowRightLong className="w-4 h-4" />
+          </div>
+
+        </div>
+
+        <section className="grid items-center grid-cols-1 gap-8 align-middle md:w-3/5 md:grid-cols-2">
+          <div className="">
+            <IoHelpBuoyOutline className="w-8 h-8 text-[#8F2D26]" />
+            <h4 className="font-bold text-[#8F2D26]">Safety</h4>
             <p>We prioritise safety across all our services </p>
           </div>
 
-          <div className="flex-[1/2]">
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">Environment Safe</h4>
+          <div className="">
+            <PiDropBold className="w-8 h-8 text-[#8F2D26]" />
+            <h4 className="font-bold text-[#8F2D26]">Environment Safe</h4>
             <p>We prioritise safety across all our services </p>
           </div>
 
           <div>
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">On Schedule</h4>
+            <IoMdAlarm className="w-8 h-8 text-[#8F2D26]" />
+            <h4 className="font-bold text-[#8F2D26]">On Schedule</h4>
             <p>We prioritise safety across all our services </p>
           </div>
 
           <div>
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">Under Budget</h4>
+            <IoDiamondOutline className="w-8 h-8 text-[#8F2D26]" />
+            <h4 className="font-bold text-[#8F2D26]">Under Budget</h4>
             <p>We prioritise safety across all our services </p>
           </div>
 
-          <div className="flex items-center text-red-800">
+          <div className="flex items-center text-[#8F2D26] md:hidden">
             <h1>Schedule free consultation </h1>
             <MdArrowRight />
           </div>
@@ -59,60 +97,60 @@ export default function page() {
       </section>
 
       {/* Our Esteemed Clients */}
-      <section className="px-8 py-8">
+      <section className="px-8 py-8 md:px-16">
         <h1 className="text-2xl" >Our Esteem Clients</h1>
 
       </section>
 
       {/* Our Services */}
-      <section className="px-8 py-8">
-        <h1 className="text-2xl" >Our Services</h1>
-        <section className="flex flex-wrap gap-4">
-          <div className="flex-[1/2]">
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">Soil Resistively</h4>
-            <p>We prioritise safety across all our services </p>
+      <section className="px-8 py-8 md:px-16">
+        <h1 className="text-2xl text-center md:text-4xl" >Our Services</h1>
+        <section className="grid grid-cols-1 px-16 py-8 md:gap-x-4 md:grid-cols-3 md:gap-y-16 ">
+          <div className="">
+            <IoHelpBuoyOutline className="w-8 h-8 text-[#8F2D26]" />
+            <h4 className="font-bold text-[#8F2D26]">Soil Resistively</h4>
+            <p className="text-[#8F2D26]">We prioritise safety across all our services </p>
           </div>
 
-          <div className="flex-[1/2]">
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">Supply of Materials</h4>
-            <p>We prioritise safety across all our services </p>
-          </div>
-
-          <div>
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">Pin brazing with complete accessoriest</h4>
+          <div className="">
+            <IoHelpBuoyOutline className="w-8 h-8" />
+            <h4 className="font-bold text-red-800">Supply of Materials</h4>
             <p>We prioritise safety across all our services </p>
           </div>
 
           <div>
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">P.S.P Monitoring</h4>
+            <IoHelpBuoyOutline className="w-8 h-8" />
+            <h4 className="font-bold text-red-800">Pin brazing with complete accessoriest</h4>
             <p>We prioritise safety across all our services </p>
           </div>
 
           <div>
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">CIPL Survey Equipment</h4>
+            <IoHelpBuoyOutline className="w-8 h-8" />
+            <h4 className="font-bold text-red-800">P.S.P Monitoring</h4>
             <p>We prioritise safety across all our services </p>
           </div>
 
           <div>
-            <MdSafetyCheck />
-            <h4 className="text-red-800 font-bold">Pipeline Defect Mapper (PDM)</h4>
+            <IoHelpBuoyOutline className="w-8 h-8" />
+            <h4 className="font-bold text-red-800">CIPL Survey Equipment</h4>
+            <p>We prioritise safety across all our services </p>
+          </div>
+
+          <div>
+            <IoHelpBuoyOutline className="w-8 h-8" />
+            <h4 className="font-bold text-red-800">Pipeline Defect Mapper (PDM)</h4>
             <p>We prioritise safety across all our services </p>
           </div>
         </section>
       </section>
 
       {/* Statistics  */}
-      <section className="px-8 py-8">
+      <section className="px-8 py-8 md:px-16">
         <h1 className="text-2xl">
           How we have done so far
         </h1>
 
-        <section className="flex flex-col md:flex-row gap-4">
+        <section className="flex flex-col gap-4 md:flex-row">
           <div className="text-center">
             <div className="flex justify-center">
               <h1 className="text-5xl">232</h1>
@@ -150,7 +188,7 @@ export default function page() {
       </section>
 
       {/* Reliable Company  */}
-      <section className="flex flex-col items-center text-center px-8 py-8">
+      <section className="flex flex-col items-center px-8 py-8 text-center md:px-16">
         <h1 className="text-2xl">
           What makes us a reliable company ?
         </h1>
@@ -162,7 +200,7 @@ export default function page() {
       </section>
 
       {/* help you get started */}
-      <section className="flex flex-col px-8 py-8">
+      <section className="flex flex-col px-8 py-8 md:px-16">
 
         <h1 className="text-2xl">We are here to help you get started</h1>
 
@@ -196,34 +234,34 @@ export default function page() {
             id="fullName"
             type="text"
             placeholder="eg: Nikhil"
-            className="px-4 py-1 border border-solid border-gray-300 rounded-md"
+            className="px-4 py-1 border border-gray-300 border-solid rounded-md"
           />
           <label htmlFor="emailId" className="label">Email Id</label>
           <input
             id="emailId"
             type="email"
             placeholder="eg: nikhil@gmail.com"
-            className="px-4 py-1 border border-solid border-gray-300 rounded-md"
+            className="px-4 py-1 border border-gray-300 border-solid rounded-md"
           />
           <label htmlFor="country" className="label">Country</label>
           <input
             id="country"
             type="text"
             placeholder="eg: nikhil@gmail.com"
-            className="px-4 py-1 border border-solid border-gray-300 rounded-md"
+            className="px-4 py-1 border border-gray-300 border-solid rounded-md"
           />
           <label htmlFor="phoneNumber" className="label">Phone Number</label>
           <input
             id="phoneNumber"
             type="number"
             placeholder="eg: nikhil@gmail.com"
-            className="px-4 py-1 border border-solid border-gray-300 rounded-md"
+            className="px-4 py-1 border border-gray-300 border-solid rounded-md"
           />
           <label htmlFor="requirement" className="label">Requirement</label>
           <textarea
             id="requirement"
             placeholder="Write your requirements"
-            className="px-4 py-1 border border-solid border-gray-300 rounded-md"
+            className="px-4 py-1 border border-gray-300 border-solid rounded-md"
           />
         </form>
 
