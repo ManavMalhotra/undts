d3a9b622f0188ae509b300590596febf28e657f7
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2024-06-30 11:05:32 +0530

    fixed responsiveness

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -13,28 +13,30 @@ function page() {
                 <div className='flex-1'><h1 className='text-4xl'>About Us</h1></div>
                 <div className='flex-1'><p>We introduce ourselves as one of the reputed construction companies in India having registered office in Bihar and carrying out business in the following hi-tech engineering fields.</p></div>
             </section>
-            <section className='flex gap-16 flex-col my-16'>
+            <section className='flex flex-col gap-16 my-16'>
                 <Image src={project1} alt="project1" layout="responsive" />
                 <div className='w-1/2'>
                     <p>We introduce ourselves as one of the reputed construction companies in India having registered office in Bihar and carrying out business in the following hi-tech engineering fields.</p>
                 </div>
             </section>
-            <section >
-                <div className='bg-[#8F2D26] p-12 flex rounded-3xl justify-between items-center text-white'>
-                    <div className='w-1/2 flex flex-col gap-6'>
+            <section className='' >
+                <div className='bg-[#8F2D26] px-8 py-6 md:p-12 flex flex-col md:flex-row  rounded-3xl justify-between md:items-center text-white gap-4'>
+                    <div className='flex flex-col gap-6 md:w-1/2'>
                         <p className='font-normal'>
                             We introduce ourselves as one of the reputed construction companies in India having registered office in Bihar and carrying out business in the following hi-tech engineering fields.
                         </p>
-                        <div className='flex gap-4'>
+                        <div className='flex flex-row gap-4 md:flex-row'>
                             <h4>Call us at :</h4>
                             <h4>+ 1234567890</h4>
                         </div>
                     </div>
-                    <div className='max-w-max'>
-                        <Link href="/#contact-us" className="w-full px-6 py-4 text-lg text-white bg-black rounded-full flex items-center gap-2">
-                            Request a call back <FaArrowRight />
+                    <div className=''>
+                        <Link href="/#contact-us" className="flex items-center justify-center w-full gap-4 px-8 py-4 text-lg text-white bg-black rounded-full">
+                            <h3 className='text-nowrap'>
+                                Request a call back
+                            </h3>
+                            <FaArrowRight />
                         </Link>
-
                     </div>
                 </div>
             </section>
