53c1fb6981ee890e29d82425242c6e1fb88b8979
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2024-08-25 13:51:23 +0530

    refactor: Update image class name in about-us/page.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,14 +8,14 @@ import project2 from "@/app/project2.png"
 import project3 from "@/app/project3.png"
 
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
-import { FaCircleArrowRight } from "react-icons/fa6";
+import { FaCircleArrowRight, FaLocationDot } from "react-icons/fa6";
 import Container from './Container';
 
 export default function Hero() {
     return (
-        <Container className="text-center pt-0">
+        <Container className="pt-0 text-center">
             <section className="mb-16">
-                <h3 className="text-base mb-4">Since 1990</h3>
+                <h3 className="mb-4 text-base">Since 1990</h3>
                 <h1 className="text-[1.8rem] leading-10 md:text-6xl md:px-64">
                     India's most reliable corrosion prevention service company
                 </h1>
@@ -23,17 +23,23 @@ export default function Hero() {
             <Carousel>
                 <div className="embla__slide rounded-2xl">
                     <Image src={project1} alt="project1" layout="responsive" />
-                    {/* <section className="top-[1vw] left-[1vw] absolute flex-row items-start justify-between hidden w-full max-w-xs px-4 py-4 text-left bg-white shadow-lg md:flex rounded-2xl">
+
+                    <section className="top-[1vw] left-[1vw] absolute flex-row items-start justify-between hidden w-[350px] max-w-xs px-4 py-4 text-left bg-[#8F2D26] text-white shadow-lg md:flex rounded-2xl
+                    
+                    ">
                         <div className="">
-                            <h1 className="text-2xl">Project Name</h1>
+                            <div className='flex items-center gap-2 '>
+                                <FaLocationDot size={12} />
+                                <h1 className="text-xs font-bold raleway"> Hyderabad Airport</h1>
+                            </div>
                             <p className="mt-2 text-sm">
-                                We introduce ourselves as one of the reputed construction companies in India having registered office in Bihar and carrying out business in the following hi-tech engineering fields.
+                                Site Survey of We introduce ourselves as one of the reputed construction companies in India having registered.
                             </p>
                         </div>
-                        <div className="mt-4 sm:ml-4 sm:mt-0">
+                        {/* <div className="mt-4 sm:ml-4 sm:mt-0">
                             <FaCircleArrowRight className="w-6 h-6 text-red-700" />
-                        </div>
-                    </section> */}
+                        </div> */}
+                    </section>
                 </div>
                 <div className="embla__slide rounded-2xl ">
                     <Image src={project2} alt="project2" layout="responsive" />
