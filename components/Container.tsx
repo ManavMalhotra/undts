a0fab53a850317d6ce9a0dc8d9a819f730f9a4dd
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2024-08-30 19:07:10 +0530

    refactor: Update Container component py-24 to py-16

--- components/Container.tsx
+++ components/Container.tsx
@@ -9,7 +9,7 @@ interface ContainerProps {
 
 function Container({ className, id, children, ...props }: ContainerProps) {
     return (
-        <section className={cn("col-span-4 md:py-24 py-12 md:col-span-8", className)} id={id} {...props}>
+        <section className={cn("col-span-4 md:py-16 py-12 md:col-span-8", className)} id={id} {...props}>
             {children}
         </section>
     );
